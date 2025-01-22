4c55202177368cb6e67f9d674c7254707bfd9d4c
kiwicat-minecraft <emgekilian@outlook.de>
2025-01-22 16:37:55 +0100

    test

---
+++ src/main/java/net/dark/kiwi/DarkMod.java
@@ -0,0 +1,18 @@
+package net.dark.kiwi;
+//note hi
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DarkMod implements ModInitializer {
+	public static final String MOD_ID = "dark-mod";
+
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/dark/kiwi/DarkModClient.java
@@ -0,0 +1,10 @@
+package net.dark.kiwi;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DarkModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
