f95ded4e36a976ac569a2037eb4cfb946c6415e4
kiwicat-minecraft <emgekilian@outlook.de>
2025-01-23 17:37:54 +0100

    fixed new item

--- src/main/java/net/dark/kiwi/item/ModItems.java
+++ src/main/java/net/dark/kiwi/item/ModItems.java
@@ -7,10 +7,12 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    public static final Item DARK_ORE = registerItem( "dark_ore", new Item(new Item.Settings()));
+    public static final Item DARK_ORE = registerItem("dark_ore", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM,Identifier.of(DarkMod.MOD_ID,"dark_ore")))));
 
 
     private static Item registerItem(String name, Item item){
