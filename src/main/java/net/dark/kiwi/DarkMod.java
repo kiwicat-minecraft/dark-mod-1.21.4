17e6b4ce6bffb622da72053defbbbe91cef9fc8c
kiwicat-minecraft <emgekilian@outlook.de>
2025-01-23 16:53:50 +0100

    added new item

--- src/main/java/net/dark/kiwi/DarkMod.java
+++ src/main/java/net/dark/kiwi/DarkMod.java
@@ -1,5 +1,6 @@
 package net.dark.kiwi;
-//note hi
+//note hi test
+import net.dark.kiwi.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -13,6 +14,6 @@ public class DarkMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/dark/kiwi/item/ModItems.java
@@ -0,0 +1,27 @@
+package net.dark.kiwi.item;
+
+import net.dark.kiwi.DarkMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item DARK_ORE = registerItem( "dark_ore", new Item(new Item.Settings()));
+
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, Identifier.of(DarkMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems(){
+        DarkMod.LOGGER.info("Registering Mod Items for " + DarkMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(DARK_ORE);
+        });
+    }
+}
